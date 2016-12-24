6a254a8a951b138664fabb72e760c535f6979191
Grant Harris <grant@eatart.org>
2016-12-23 16:33:29 -0800

    Enable all loops

--- Firmware/battery-interface/battery-interface.ino
+++ Firmware/battery-interface/battery-interface.ino
@@ -3,20 +3,15 @@
 
 void setup(void)
 {
-
 	Serial.begin(9600);
-	
 	Scheduler.start(CAN::setup, CAN::read);
-  Scheduler.start(Inputs::setup, Inputs::read);
-
-  Scheduler.start(Indicators::setup, Indicators::update);
-  Scheduler.start(Buzzer::setup, Buzzer::update);
-
+	Scheduler.start(Inputs::setup, Inputs::read);
+	Scheduler.start(Indicators::setup, Indicators::update);
+	Scheduler.start(Buzzer::setup, Buzzer::update);
 }
 
 
 void loop(void)
 {
-
 }
 
